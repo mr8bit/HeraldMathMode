7ba98e68e9b87ab3549bc9429547395349356f59
mr8bit <a.mazaew@lambda-it.ru>
2021-05-24 14:14:41 +0300

    Обновил compose

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,4 +2,4 @@ echo "MIGRATING DATABASE"
 python manage.py migrate --settings=backend.settings.dev
 
 echo "RUN gunicorn"
-gunicorn --bind 0.0.0.0:8000 --log-level INFO backend.wsgi
+gunicorn --bind 0.0.0.0:8891 --log-level INFO backend.wsgi
