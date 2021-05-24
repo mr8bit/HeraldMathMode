bf32d9c3fa340a2f646632d3d386c95701bec6bf
mr8bit <a.mazaew@lambda-it.ru>
2021-05-24 22:00:04 +0300

    Обновил compose

--- backend/urls.py
+++ backend/urls.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
-
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/', include('backend.admin.urls')),
-    path('api/', include('backend.schedule.urls')),
-    path('', include('backend.bot.urls')),
-]
+                  path('admin/', admin.site.urls),
+                  path('api/', include('backend.admin.urls')),
+                  path('api/', include('backend.schedule.urls')),
+                  path('', include('backend.bot.urls')),
+              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
